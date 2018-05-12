23c8febdfcf289e890fcfb834a6eba69df9d7fd7
刘少甫 <liushaofu>
2018-05-12 14:47:54 +0800

    up

--- src/session9/util/util.java
+++ src/session9/util/util.java
@@ -4,6 +4,7 @@ package session9.util;
  * Created by Administrator on 2018/5/4.
  */
 public class util {
+    private util(){};//防止实例化
     public static final void swap(int[] list,int a,int b){
             int temp=list[a];
             list[a]=list[b];
